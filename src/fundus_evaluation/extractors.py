aae83a644dd17d9836c24879794210412ac771fd
Conrad Dobberstein <29147025+dobbersc@users.noreply.github.com>
2024-03-07 02:11:08 +0100

    Add fundus extractor

---
+++ src/fundus_evaluation/extractors.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from typing import Any, Dict, List, Protocol
+
+
+class Extractor(Protocol):
+    """Protocol for extraction functions."""
+
+    def __call__(self, html: str, publisher_identifier: str, crawl_date: datetime) -> List[str]:
+        ...
+
+
+def extract_fundus(html: str, publisher_identifier: str, crawl_date: datetime) -> List[str]:
+    from fundus import PublisherCollection
+    from fundus.publishers.base_objects import PublisherEnum
+
+    publisher: PublisherEnum = PublisherCollection[publisher_identifier]
+    parsed_data: Dict[str, Any] = publisher.parser(crawl_date).parse(html, error_handling="raise")
+    return list(parsed_data["body"].as_text_sequence())

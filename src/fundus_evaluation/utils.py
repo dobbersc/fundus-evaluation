04fd713cb6c7a2f8e84225fbff51db55332c14f4
Conrad Dobberstein <29147025+dobbersc@users.noreply.github.com>
2024-03-08 00:55:55 +0100

    Add utility functions to - handle optional paragraphs - compute ngrams

--- src/fundus_evaluation/utils.py
+++ src/fundus_evaluation/utils.py
@@ -1,6 +1,8 @@
 import json
 from pathlib import Path
-from typing import TypedDict, List, Union, Dict
+from typing import Dict, Iterable, Iterator, List, Tuple, TypedDict, Union
+
+import more_itertools
 
 
 class EvaluationArticle(TypedDict):
@@ -22,6 +24,31 @@ def load_evaluation_articles(path: Union[str, Path]) -> Dict[str, EvaluationArti
     """
     with open(path, "r", encoding="utf-8") as f:
         return dict(sorted(json.load(f).items()))
+
+
+def is_optional_paragraph(paragraph: str) -> bool:
+    return paragraph[0] == "[" and paragraph[-1] == "]"
+
+
+def remove_optional_paragraph_marker(paragraph: str) -> str:
+    assert is_optional_paragraph(paragraph)
+    return paragraph[1:-1]
+
+
+def get_reference_bodies(body: List[str]) -> Iterator[List[str]]:
+    optional_paragraph_indices: Tuple[int, ...] = tuple(
+        index for index, paragraph in enumerate(body) if is_optional_paragraph(paragraph)
+    )
+    for split_indices in more_itertools.powerset(optional_paragraph_indices):
+        yield [
+            remove_optional_paragraph_marker(paragraph) if index in optional_paragraph_indices else paragraph
+            for index, paragraph in enumerate(body)
+            if index not in split_indices
+        ]
+
+
+def compute_ngrams(sequence: Iterable[str], n: int) -> Iterator[Tuple[str, ...]]:
+    yield from more_itertools.windowed(sequence, n=n, fillvalue="")
 
 
 def normalize_whitespaces(text: str) -> str:

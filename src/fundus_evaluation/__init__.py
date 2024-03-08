69463d3440e7ed0be38817947964606aba104903
Conrad Dobberstein <29147025+dobbersc@users.noreply.github.com>
2024-03-08 01:01:14 +0100

    Add supported scorers

--- src/fundus_evaluation/__init__.py
+++ src/fundus_evaluation/__init__.py
@@ -2,6 +2,7 @@ import inspect
 from typing import Dict, Final
 
 import fundus_evaluation
+from fundus_evaluation.scorers import Scorer
 from fundus_evaluation.scrapers import Scraper
 
 __version__ = "1.0.0"
@@ -9,6 +10,13 @@ __version__ = "1.0.0"
 SCRAPER_PREFIX: Final[str] = "scrape_"
 SCRAPERS: Dict[str, Scraper] = {
     name[len(SCRAPER_PREFIX) :]: function
-    for name, function in inspect.getmembers(fundus_evaluation.scrapers, inspect.isfunction)
+    for name, function in inspect.getmembers(fundus_evaluation.scrapers, lambda x: isinstance(x, Scraper))
     if name.startswith(SCRAPER_PREFIX)
+}
+
+SCORER_PREFIX: Final[str] = "score_"
+SCORERS: Dict[str, Scorer] = {
+    name[len(SCORER_PREFIX) :]: function
+    for name, function in inspect.getmembers(fundus_evaluation.scorers, lambda x: isinstance(x, Scorer))
+    if name.startswith(SCORER_PREFIX)
 }
